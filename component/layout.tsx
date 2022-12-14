9b1823056981b084819b7de235023f71b9672a9e
Htet Aung Shine <h@shine.rocks>
2022-12-14 20:53:21 +1100

    Ensure the body of the blog fills the remaining page space

--- component/layout.tsx
+++ component/layout.tsx
@@ -6,6 +6,7 @@ interface Props {
   link: string,
   text: string
 }
+
 function FooterLink({ link, text }: Props) {
   return (
     <Text fontSize="xl" fontWeight="light" fontFamily="body" color="gray.400" py="1">
@@ -13,42 +14,44 @@ function FooterLink({ link, text }: Props) {
         {text}
       </a>
     </Text>
-  )
+  );
 }
 
 export default function Layout({ children }) {
   return (
     <ChakraProvider theme={theme}>
-      <Box bgGradient="linear(to-r, orange.500, yellow.300)" height="1" />
-      <Box bg="gray.900" pb="10">
-        <Container maxWidth="container.xl">
-          <Text bgGradient="linear(to-r, orange.500, yellow.300)" bgClip="text"
-                decoration="none" fontFamily="body" py="2" fontSize="lg">
-            <Link href="/">
-              SHINE.ROCKS
-            </Link>
-          </Text>
-          <Box>
-            <VStack align="left" spacing="-2" py="28">
-              <Text fontSize="6xl" color="gray.50" fontWeight="bold" fontFamily="heading">
-                Hi, my name is
-                <Text as="span" bgClip="text" color="#0A9396" decoration="none"> Shine</Text>
-              </Text>
-              <Text fontSize="xl" fontWeight="light" color="gray.200" fontFamily="body">
-                I'm a senior software engineer and this is my latest attempt at making a personal website.
-              </Text>
-            </VStack>
-          </Box>
-          {children}
-        </Container>
-      </Box>
-      <Box bg="gray.800" py="4">
-        <Container maxWidth="container.xl">
-          <FooterLink link={"https://twitter.com/_sha94"} text={"Twitter"} />
-          <FooterLink link={"https://github.com/ha-shine"} text={"GitHub"} />
-          <FooterLink link={"https://www.linkedin.com/in/htet-aung-shine-79654192/"} text={"LinkedIn"} />
-          <FooterLink link={"mailto:h@shine.rocks"} text={"h@shine.rocks"} />
-        </Container>
+      <Box minH="100vh" display="flex" flexDir="column">
+        <Box bgGradient="linear(to-r, orange.500, yellow.300)" height="1" />
+        <Box bg="gray.900" pb="10" flex="1 1 auto">
+          <Container maxWidth="container.xl">
+            <Text bgGradient="linear(to-r, orange.500, yellow.300)" bgClip="text"
+                  decoration="none" fontFamily="body" py="2" fontSize="lg">
+              <Link href="/">
+                SHINE.ROCKS
+              </Link>
+            </Text>
+            <Box>
+              <VStack align="left" spacing="-2" py="28">
+                <Text fontSize="6xl" color="gray.50" fontWeight="bold" fontFamily="heading">
+                  Hi, my name is
+                  <Text as="span" bgClip="text" color="#0A9396" decoration="none"> Shine</Text>
+                </Text>
+                <Text fontSize="xl" fontWeight="light" color="gray.200" fontFamily="body">
+                  I'm a senior software engineer and this is my latest attempt at making a personal website.
+                </Text>
+              </VStack>
+            </Box>
+            {children}
+          </Container>
+        </Box>
+        <Box bg="gray.800" py="4">
+          <Container maxWidth="container.xl">
+            <FooterLink link={"https://twitter.com/_sha94"} text={"Twitter"} />
+            <FooterLink link={"https://github.com/ha-shine"} text={"GitHub"} />
+            <FooterLink link={"https://www.linkedin.com/in/htet-aung-shine-79654192/"} text={"LinkedIn"} />
+            <FooterLink link={"mailto:h@shine.rocks"} text={"h@shine.rocks"} />
+          </Container>
+        </Box>
       </Box>
     </ChakraProvider>
   );
